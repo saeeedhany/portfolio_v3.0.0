5e8419cf3c517d5682bc16e7bcda04f6eeacd69f
saeed <alsaeedalbasi0ny@gmail.com>
2024-12-23 16:14:03 +0200

    make the click function to list the menu in the smaller devices

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let list = document.getElementById('list');
+let toggle = document.getElementById('toggle');
 
-
-
+function toggleMenu() {
+    if (list.style.display === 'block') {
+        list.style.display = 'none';
+    } else {
+        list.style.display = 'block';
+    }
+}
